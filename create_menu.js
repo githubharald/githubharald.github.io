2b291e8d841eee816cd7e5040e621008793b4af4
Harald Scheidl <harald_scheidl@hotmail.com>
2020-10-29 22:38:13 +0100

    reworked website

---
+++ create_menu.js
@@ -0,0 +1,19 @@
+s = '\
+<span class="pure-menu-heading">NAVIGATION</span>\
+<ul class="pure-menu-list">\
+    <li class="pure-menu-item">\
+        <a href="index.html" class="pure-menu-link">Home</a>\
+    </li>\
+    <li class="pure-menu-item">\
+        <a href="repos.html" class="pure-menu-link">Repositories</a>\
+    </li>\
+    <li class="pure-menu-item">\
+        <a href="publications.html" class="pure-menu-link">Publications</a>\
+    </li>\
+    <li class="pure-menu-item">\
+        <a href="articles.html" class="pure-menu-link">Articles</a>\
+    </li>\
+</ul>\
+';
+
+document.getElementById("menu").innerHTML = s;
